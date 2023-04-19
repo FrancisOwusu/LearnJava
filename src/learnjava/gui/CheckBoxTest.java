b79b8ec0d6010a4e63e29b6baadfd5ffc0931ae5
FrancisOwusu <osei1257@gmail.com>
2023-04-19 07:30:11 +0100

    JCheckbox added

---
+++ src/learnjava/gui/CheckBoxTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package learnjava.gui;
+
+/**
+ *
+ * @author HP
+ */
+public class CheckBoxTest {
+    public static void main(String[] args) {
+        CheckBoxFrame checkboxframe = new CheckBoxFrame();
+        checkboxframe.setDefaultCloseOperation(JButtonFrame.EXIT_ON_CLOSE);
+        checkboxframe.setSize(275,100);
+        checkboxframe.setVisible(true);
+        
+    }
+}
