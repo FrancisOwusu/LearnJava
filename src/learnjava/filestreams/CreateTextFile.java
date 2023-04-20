0b89c39681893276c0bf50f260ad2ff0d4c322d4
FrancisOwusu <osei1257@gmail.com>
2023-04-20 13:46:37 +0100

    create file class added

--- src/learnjava/filestreams/CreateTextFile.java
+++ src/learnjava/filestreams/CreateTextFile.java
@@ -7,6 +7,9 @@ package learnjava.filestreams;
 import java.io.FileNotFoundException;
 import java.nio.file.FileSystemNotFoundException;
 import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,12 +23,14 @@ public class CreateTextFile {
 
     public static void main(String[] args) {
         openFile();
+        addRecords();
+        closeFile();
     }
 
     // open file clients.txt
     public static void openFile() {
         try {
-output = new Formatter("clients.txt"); // open the file
+            output = new Formatter("clients.txt"); // open the file
         } catch (SecurityException securityException) {
             System.err.println("Write permission denied. Terminating.");
             System.exit(1); // terminate the program
@@ -34,6 +39,36 @@ output = new Formatter("clients.txt"); // open the file
             System.exit(1); // terminate the program
         } catch (FileNotFoundException ex) {
             Logger.getLogger(CreateTextFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void addRecords() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("%s%n%s%n? ",
+                "Enter account number, first name, last name and balance.",
+                "Enter end-of-file indicator to end input.");
+
+        while (input.hasNext()) { // loop until end-of-file indicator
+            try {
+                // output new record to file; assumes valid input
+                output.format("%d %s %s %.2f%n", input.nextInt(),
+                        input.next(), input.next(), input.nextDouble());
+            } catch (FormatterClosedException formatterClosedException) {
+
+                System.err.println("Error writing to file. Terminating.");
+                break;
+            } catch (NoSuchElementException elementException) {
+                System.err.println("Invalid input. Please try again.");
+                input.nextLine(); // discard input so user can try again
+            }
+            System.out.print("? ");
+        } //end of while
+    }//end of method addRecords
+
+    public static void closeFile(){
+        if(output !=null){
+            output.close();
         }
     }
 }
